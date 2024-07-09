0257601a76e777b8293773cb7cefd3ad23ddb1e5
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-09 11:05:24 -0700

    Added styling 💅

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 import * as THREE from "three";
+import "./style.css";
 
 //create a new scene
 const scene = new THREE.Scene();
 
+//sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 //create a sphere
-const geometry = new THREE.SphereGeometry(3, 64, 64);
+const geometry = new THREE.SphereGeometry(3, 128, 64);
 const material = new THREE.MeshStandardMaterial({ color: "0xffff" });
 const mesh = new THREE.Mesh(geometry, material);
 
 //initializing camera
-const camera = new THREE.PerspectiveCamera(35, 800 / 600, 0.1, 100);
-camera.position.z = 100;
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
 
 const light = new THREE.PointLight(0xffffff, 150, 100);
-light.position.set(10, 10, 10);
+light.position.set(0, 10, 10);
 
 //helper
 // const sphereSize = 1;
@@ -28,5 +40,5 @@ scene.add(camera);
 //renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
