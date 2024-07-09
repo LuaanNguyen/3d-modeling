a99ef19baceed46e8225b100bcb0f57a5b168cfc
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-09 11:43:35 -0700

    Added Navbar, Texts 💬

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 //create a new scene
 const scene = new THREE.Scene();
@@ -12,7 +13,7 @@ const sizes = {
 
 //create a sphere
 const geometry = new THREE.SphereGeometry(3, 128, 64);
-const material = new THREE.MeshStandardMaterial({ color: "0xffff" });
+const material = new THREE.MeshStandardMaterial({ color: "#ffffff" });
 const mesh = new THREE.Mesh(geometry, material);
 
 //initializing camera
@@ -42,3 +43,28 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+//Resizing
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+renderer.setPixelRatio(4);
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
